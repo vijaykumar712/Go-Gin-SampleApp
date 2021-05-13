21140ffd54d35c57e740ba6dacd89b88c7ac2c8b
Lenovo <vijay071295@gmail.com>
2021-05-13 08:33:36 +0530

    adding article handlers

--- routes.go
+++ routes.go
@@ -4,4 +4,7 @@ func initializeRoutes() {
 
 	// Handle the index route
 	router.GET("/", showIndexPage)
+
+	  // Handle GET requests at /article/view/some_article_id
+	router.GET("/article/view/:article_id", getArticle)
 }
